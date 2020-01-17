e9629f8a86f03c3c7aa7285254254dc1b062a216
andy1.huang[黃有進] <andy1.huang@andy1huang-mac13.local>
2020-01-17 09:50:36 +0800

    update

--- BlueGreen/awss3rsync.sh
+++ BlueGreen/awss3rsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p /tmp/s3sync
-aws s3 sync s3://andy1huang-test-travis/BlueGreen /tmp/s3sync
+aws s3 sync s3://andy1huang-test-travis /tmp/s3sync
 cp /tmp/s3sync/index.html /var/www/html/index.nginx-debian.html
 nginx -s reload
 rm -rf /tmp/s3sync
